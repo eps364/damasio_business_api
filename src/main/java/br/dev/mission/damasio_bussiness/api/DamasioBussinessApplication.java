914f1dda848bf209461fe57a581654b5e940eb6b
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2022-10-01 19:56:17 -0300

    first commit

---
+++ src/main/java/br/dev/mission/damasio_bussiness/api/DamasioBussinessApplication.java
@@ -0,0 +1,15 @@
+package br.dev.mission.damasio_bussiness.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamasioBussinessApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DamasioBussinessApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/br/dev/mission/damasio_bussiness/api/DamasioBussinessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mission.damasio_bussiness.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamasioBussinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
